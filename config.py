e86f7c30e7617199cfd5c3b449e34f3c7c2c2893
Andresval185 <0253783@up.edu.mx>
2026-05-22 13:11:31 -0400

    Configuración inicial del proyecto variables de entorno, base de datos MySQL y clave secreta de Flask

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'deskmate-secret-key-2025'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://root:@localhost/deskmate'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
